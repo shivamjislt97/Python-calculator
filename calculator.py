d2d35cf7b7c8a1bca74df03ba60b59029e6914f4
Shivamji549 <shivamji549@gmail.com>
2026-07-01 21:34:29 +0530

    finally done and error proof

---
+++ calculator.py
@@ -0,0 +1,27 @@
+num1 = int(input("Enter the number : "))
+num2 = 0
+result = num1
+while True:
+
+    operator = input("Enter the operator (+, -, *, /): and press = to exit: ")
+    if (operator == '='):
+        break
+
+    num2 = int(input("Enter the number: "))
+
+    if (operator == '+'):
+        result = num1 + num2
+
+    if (operator == '-'):
+        result = num1 - num2
+
+    if (operator == '*'):
+        result = num1 * num2
+
+    if (operator == '/'):
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+            continue
+        result = num1 / num2
+
+print("Result: ", result)
